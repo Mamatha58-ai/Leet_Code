af224b3462ce994304a76a875c077278a37433f2
Mamatha Kothacheruvu <24b11cs229@adityauniversity.in>
2025-05-04 21:21:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (22.66%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,14 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int index = 0;
+    for(int i = 0 ; i< numsSize ; i++){
+        if(nums[i]==target){
+            index = i ;
+        }
+        else {
+            if(nums[i]<target){
+                index = i+1;
+            }
+        }
+    }
+    return index ;
+}
