2928f604c22fd7f0e904f73a55d2a7108b42ff63
Mamatha Kothacheruvu <24b11cs229@adityauniversity.in>
2026-03-14 21:17:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+           unordered_map<char, int>mp;
+        if(s.length()!=t.length()) return false;
+
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]--;
+            mp[t[i]]++;
+        }
+
+        for(auto it : mp){
+            if(it.second!=0){
+                return false;
+            }
+        }
+        return true;
     }
 };
